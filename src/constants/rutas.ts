50159b6970c4f449c0e91b1803068f0ee08aa91d
Leo0114 <leonel0614@gmail.com>
2025-01-28 14:13:48 -0600

    ft:TAOSTry

--- src/constants/rutas.ts
+++ src/constants/rutas.ts
@@ -1,5 +1,6 @@
 export const navbarRoutes = [
   { name: "Inicio", path: "/" },
+  { name: "Productos", path: "/products/" },
   { name: "Nosotros", path: "/about/" },
   { name: "Contacto", path: "/contact/" },
 ];
---
+++ src/constants/team.ts
@@ -0,0 +1,64 @@
+import image from "@assets/image.webp";
+
+export const OurTeam = [
+  {
+    name: "John Doe",
+    role: "CEO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Jane Doe",
+    role: "CTO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Alice Doe",
+    role: "COO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Bob Doe",
+    role: "CFO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Eve Doe",
+    role: "CMO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Charlie Doe",
+    role: "CIO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Grace Doe",
+    role: "CPO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Hank Doe",
+    role: "CBO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Ivy Doe",
+    role: "CLO",
+    image: image,
+    route: "#",
+  },
+  {
+    name: "Jack Doe",
+    role: "CDO",
+    image: image,
+    route: "#",
+  },
+];
