e44538d350a1a1b2b178281ac4a57027539513b6
Leo0114 <leonel0614@gmail.com>
2025-01-29 23:12:41 -0600

    ft:cambios

--- src/constants/rutas.ts
+++ src/constants/rutas.ts
@@ -1,6 +1,5 @@
 export const navbarRoutes = [
   { name: "Inicio", path: "/" },
-  { name: "Productos", path: "/products/" },
   { name: "Nosotros", path: "/about/" },
   { name: "Contacto", path: "/contact/" },
 ];
